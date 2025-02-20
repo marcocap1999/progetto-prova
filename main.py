e85b1a90b5c990aaf0f85523fe2ed1baf3dfe530
alimonti99 <alice.monti.am@gmail.com>
2025-02-20 12:38:35 +0100

    Update main.py
    Insertion of comments for the creation of the main code

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-# Main file to write the project
+# List of choices
+
+# Get the computer's choice
+
+# Ask the user for their choice
+
+# Check if the user's choice is valid
+
+    # Display the choices
+    # Compare the choices
